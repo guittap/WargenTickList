72ea2489403183dc6520ef420a8412f49128e3b4
Wargen Guittap <48695406+guittap@users.noreply.github.com>
2022-09-25 20:32:54 -0700

    (╯•ᗣ•╰)

--- ticklist/src/App.js
+++ ticklist/src/App.js
@@ -3,17 +3,17 @@ import Home from "./Home";
 import Kraft from "./areas/Kraft";
 import RedSprings from "./areas/RedSprings";
 import PriestDraw from "./areas/PriestDraw";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Kraft" element={<Kraft/>}/>
         <Route path="/RedSprings" element={<RedSprings/>}/>
         <Route path="/PriestDraw" element={<PriestDraw/>}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
